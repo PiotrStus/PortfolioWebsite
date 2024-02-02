2c2038877c67391bad47eef447cad1982b86d236
Piotr Stus <pm2024love@gmail.com>
2024-02-02 15:42:37 +0100

    Navbar function & styling  added

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -4,11 +4,12 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Contact from './Pages/Contact/Contact';
-
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/omnie" element={<About />}></Route>
---
+++ react-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import classes from "./Navbar.module.css"
+import {Link , useMatch, useResolvedPath} from "react-router-dom"
+
+
+function Navbar() {
+    return (<nav className={classes.nav}>
+        <ul>
+            <CustomLink to="/omnie" className={classes.listElement}>O mnie</CustomLink>
+            <CustomLink to="/portfolio" className={classes.listElement}>Portfolio</CustomLink>
+            <CustomLink to="/kontakt" className={classes.listElement}>Kontakt</CustomLink>
+            <Link to="https://filmpolski.pl/fp/index.php?osoba=11198905" target="_blank" rel="noopener noreferrer"  className={classes.listElement}>FilmPolski.pl</Link>
+        </ul>
+        <Link to="/" className={classes.siteTitle}>Magdalena Tarnawska-Stus
+        </Link>
+    </nav>
+    )
+}
+
+function CustomLink({to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    return (
+        <li className={isActive ? classes.navItemActive : ""}>
+            
+            <Link to={to} {...props}>{children}
+            </Link>
+        </li>
+    )
+}
+
+export default Navbar;
