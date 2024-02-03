f70bfd624b2e90858f1ef9690e5d0f9c086a07dd
Piotr Stus <pm2024love@gmail.com>
2024-02-03 22:59:40 +0100

    Contact icon link, css.moduel added.

--- react-project/src/Pages/Contact/Contact.jsx
+++ react-project/src/Pages/Contact/Contact.jsx
@@ -1,7 +1,13 @@
+import instagramLogo from '..//../assets/instagram.svg'
+import classes from "./Contact.module.css"
+
 function Contact() {
 return (
     <>
         <h1>Contact</h1>
+        <div>
+            <a href="https://www.instagram.com/magdalena_tarnawska/"><img src={instagramLogo} className={classes.logo} alt="Instagram logo" /></a>
+        </div>
     </>
 )}
 
